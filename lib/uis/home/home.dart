6880b731015807fef0e0d36af7022d7f14b5918d
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-01-02 18:52:57 +0300

    Started main app layout design..

--- lib/uis/home/home.dart
+++ lib/uis/home/home.dart
@@ -1,8 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CupertinoPageScaffold(
+      child: Container(
+        color: Colors.red,
+      ),
+    );
   }
 }
