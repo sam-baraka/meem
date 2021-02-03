a10ba567ba5a63415c36397a5041d6a70f60b2ba
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-03 18:56:28 +0300

    Wrapped the app with the bloc providers

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart' as hydratedBloc;
 import 'package:flutter_bloc/flutter_bloc.dart' as bloc;
 import 'package:meem/bloc/simple_bloc_observer.dart';
+import 'package:meem/cubits/cubits.dart';
 import 'package:meem/uis/intro/intro_screen.dart';
 
 void main() async {
@@ -10,7 +11,9 @@ void main() async {
   hydratedBloc.HydratedBloc.storage =
       await hydratedBloc.HydratedStorage.build();
   bloc.Bloc.observer = SimpleBlocObserver();
-  runApp(MeemApp());
+  runApp(bloc.MultiBlocProvider(providers: [
+    bloc.BlocProvider<TabCubit>(create: (context) => TabCubit(0)),
+  ], child: MeemApp()));
 }
 
 class MeemApp extends StatelessWidget {
