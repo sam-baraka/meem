195012d683a6f8b776846e127996d2df2324385d
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-03 18:31:38 +0300

    Initialized hydrated bloc for future use

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:meem/uis/intro/intro_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build();
   runApp(MeemApp());
 }
 
