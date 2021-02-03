8fcd978dc88e3f0d6ba09d9147dfc4464b23288b
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-03 19:09:42 +0300

    Added dummy text to show which page I'm on

--- lib/uis/tabs/create_tab.dart
+++ lib/uis/tabs/create_tab.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class CreateTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Text("Create"),
+    );
   }
 }
--- lib/uis/tabs/home_tab.dart
+++ lib/uis/tabs/home_tab.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Text("Home"),
+    );
   }
 }
