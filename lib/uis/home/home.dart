5539659d2b6df5f721fa0dfa36b4003a3b18b98a
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-03 18:48:54 +0300

    Implemented on change tabs with cubit

--- lib/uis/home/home.dart
+++ lib/uis/home/home.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meem/cubits/cubits.dart';
+import 'package:meem/uis/uis.dart';
 
 class Home extends StatelessWidget {
+  List<Widget> tabs = [HomeTab(), CreateTab(), SettingsTab()];
   @override
   Widget build(BuildContext context) {
+    TabCubit tabCubit = BlocProvider.of<TabCubit>(context);
     return CupertinoTabScaffold(
       tabBuilder: (BuildContext context, int index) {
-        return Container(
-          child: Center(child: Text("$index"),),
-        );
+        return tabs[index];
       },
       tabBar: CupertinoTabBar(
+        onTap: (value) {
+          tabCubit.changeTab(index: value);
+        },
         items: [
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.home),label: "Home"),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.add_circled),label: "Create"),
-          BottomNavigationBarItem(icon: Icon(CupertinoIcons.settings),label: "Settings"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.add_circled), label: "Create"),
+          BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.settings), label: "Settings"),
         ],
       ),
     );
