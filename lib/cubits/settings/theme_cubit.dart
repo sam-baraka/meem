1fbba30c619f9b1dbf293348e58056f8b556da3c
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-04 22:30:50 +0300

    Implemented change theme

--- lib/cubits/settings/theme_cubit.dart
+++ lib/cubits/settings/theme_cubit.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 
 class ThemeCubit extends HydratedCubit<ThemeData> {
-  ThemeCubit(ThemeData state) : super(state);
+  ThemeCubit(ThemeData state) : super(ThemeData.light());
+
+  changeTheme(bool value) {
+    if (value == true) {
+      emit(ThemeData.dark());
+    } else if (value == false) {
+      emit(ThemeData.light());
+    }
+  }
 
   @override
   ThemeData fromJson(Map<String, dynamic> json) => json['value'];
