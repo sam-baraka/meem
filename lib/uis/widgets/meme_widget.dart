27e2dec9ddc54c2e6be0a71ce9d7d03f761c6327
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-05 00:50:03 +0300

    WIP: Meme COntainer

--- lib/uis/widgets/meme_widget.dart
+++ lib/uis/widgets/meme_widget.dart
@@ -1,8 +1,31 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MemeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Stack(
+      children: [
+        Positioned(
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Image.asset(
+            'assets/meme/funny_dog.jpg',
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+        Positioned(
+          top: 0,
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: CupertinoPopupSurface(
+            isSurfacePainted: true,
+          ),
+        ),
+      ],
+    );
   }
 }
