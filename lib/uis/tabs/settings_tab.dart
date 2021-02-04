c486ca6b80021719758cfa4a0e8fe2b0022b2e72
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-04 22:43:40 +0300

    Added settings dark theme switch

--- lib/uis/tabs/settings_tab.dart
+++ lib/uis/tabs/settings_tab.dart
@@ -1,10 +1,26 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:meem/cubits/cubits.dart';
 
 class SettingsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Settings"),
+    return ListView(
+      physics: BouncingScrollPhysics(),
+      children: [
+        ListTile(
+          title: Text("Dark theme"),
+          subtitle: Text("Toggle between dark and light theme"),
+          trailing: CupertinoSwitch(
+            value: context.watch<ThemeCubit>().state == ThemeData.dark(),
+            onChanged: (value) {
+              context.read<ThemeCubit>().changeTheme(value);
+            },
+          ),
+        )
+      ],
     );
   }
 }
