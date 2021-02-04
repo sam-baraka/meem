2281d25516eb7d7be81f880e1d70c0613ad757b9
Samuel Baraka <35136541+sababuvercetti@users.noreply.github.com>
2021-02-04 23:02:00 +0300

    Added home tab pageview? layout

--- lib/uis/tabs/home_tab.dart
+++ lib/uis/tabs/home_tab.dart
@@ -3,8 +3,32 @@ import 'package:flutter/material.dart';
 class HomeTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Home"),
+    return PageView(
+      scrollDirection: Axis.vertical,
+      physics: BouncingScrollPhysics(),
+      children: [
+        Container(
+          color: Colors.red,
+        ),
+        Container(
+          color: Colors.orange,
+        ),
+        Container(
+          color: Colors.yellow,
+        ),
+        Container(
+          color: Colors.green,
+        ),
+        Container(
+          color: Colors.blue,
+        ),
+        Container(
+          color: Colors.indigo,
+        ),
+        Container(
+          color: Colors.cyan,
+        ),
+      ],
     );
   }
 }
