554c061844ebd59b48a8598b7439780b912615c5
iampato <858wpwaweru@gmail.com>
2021-06-22 20:38:13 +0300

    updated flutterbloc plus breaking changes

--- lib/bloc/simple_bloc_observer.dart
+++ lib/bloc/simple_bloc_observer.dart
@@ -1,28 +1,29 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 /// Custom [BlocObserver] which observes all bloc and cubit instances.
 class SimpleBlocObserver extends BlocObserver {
   @override
-  void onEvent(Bloc bloc, Object event) {
-    print(event);
-    super.onEvent(bloc, event);
+  void onCreate(BlocBase cubit) {
+    super.onCreate(cubit);
+    debugPrint('onCreate -- cubit: ${cubit.runtimeType}');
   }
 
   @override
-  void onChange(Cubit cubit, Change change) {
-    print(change);
+  void onChange(BlocBase cubit, Change change) {
     super.onChange(cubit, change);
+    debugPrint('onChange -- cubit: ${cubit.runtimeType}, change: $change');
   }
 
   @override
-  void onTransition(Bloc bloc, Transition transition) {
-    print(transition);
-    super.onTransition(bloc, transition);
-  }
-
-  @override
-  void onError(Cubit cubit, Object error, StackTrace stackTrace) {
-    print(error);
+  void onError(BlocBase cubit, Object error, StackTrace stackTrace) {
+    debugPrint('onError -- cubit: ${cubit.runtimeType}, error: $error');
     super.onError(cubit, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase cubit) {
+    super.onClose(cubit);
+    debugPrint('onClose -- cubit: ${cubit.runtimeType}');
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart' as hydratedBloc;
 import 'package:flutter_bloc/flutter_bloc.dart' as bloc;
 import 'package:meem/bloc/simple_bloc_observer.dart';
 import 'package:meem/cubits/cubits.dart';
 import 'package:meem/uis/intro/intro_screen.dart';
+import 'package:path_provider/path_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  hydratedBloc.HydratedBloc.storage =
-      await hydratedBloc.HydratedStorage.build();
+  hydratedBloc.HydratedBloc.storage = await hydratedBloc.HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? hydratedBloc.HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
   bloc.Bloc.observer = SimpleBlocObserver();
   runApp(bloc.MultiBlocProvider(providers: [
     bloc.BlocProvider<TabCubit>(create: (context) => TabCubit(0)),
