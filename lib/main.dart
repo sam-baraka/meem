d1200ae2d88f3ea19842e6e54ecd2d279fd87c89
iampato <858wpwaweru@gmail.com>
2021-06-22 20:42:34 +0300

    format code

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,19 @@ void main() async {
         : await getTemporaryDirectory(),
   );
   bloc.Bloc.observer = SimpleBlocObserver();
-  runApp(bloc.MultiBlocProvider(providers: [
-    bloc.BlocProvider<TabCubit>(create: (context) => TabCubit(0)),
-    bloc.BlocProvider<ThemeCubit>(
-        create: (context) => ThemeCubit(ThemeData.light())),
-  ], child: MeemApp()));
+  runApp(
+    bloc.MultiBlocProvider(
+      providers: [
+        bloc.BlocProvider<TabCubit>(create: (context) => TabCubit(0)),
+        bloc.BlocProvider<ThemeCubit>(
+          create: (context) => ThemeCubit(
+            ThemeData.light(),
+          ),
+        ),
+      ],
+      child: MeemApp(),
+    ),
+  );
 }
 
 class MeemApp extends StatelessWidget {
